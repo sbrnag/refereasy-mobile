32dd79d47d6942a8b1cdf6eb5338bbfe482896b6
nagendra prasad sbr <nagendra.sbr@gmail.com>
2016-02-25 17:09:35 +0530

    chat fixs

--- www/js/controllers/chatDetail.js
+++ www/js/controllers/chatDetail.js
@@ -1,23 +1,25 @@
-app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats, Auth, $timeout, $ionicScrollDelegate) {
+app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats, Auth, $timeout, 
+                                            $ionicScrollDelegate, $rootScope, $ionicPlatform) {
 
   $scope.currentUser = Auth.user;
-  var isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
-
-  var viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
-  
   $scope.chat = Chats.get($stateParams.chatId);
   $scope.messages = Chats.getMessages($stateParams.chatId);
 
+  
+  var isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+
+  //var viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
+  
+  
+
   $scope.messages.$loaded(function() {
-      $timeout(function() {
-        viewScroll.scrollBottom();
-      }, 0);
+    $ionicScrollDelegate.scrollBottom();
   });
 
   $scope.messages.$watch(function() { 
     $timeout(function() {
-        viewScroll.scrollBottom();
-    }, 0);
+        $ionicScrollDelegate.scrollBottom();
+    }, 300);
   });
 
   $scope.sendMessage = function() {
@@ -25,11 +27,6 @@ app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats, Auth, $ti
 
     //to make the messaging fast adding the logic here itself
     var currTimeStamp = Firebase.ServerValue.TIMESTAMP;
-
-    //saving chat details
-    $scope.chat.datetime = currTimeStamp;
-    $scope.chat.lastText = $scope.text;
-    $scope.chat.$save();
 
     //saving message
     $scope.messages.$add({
@@ -39,26 +36,83 @@ app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats, Auth, $ti
           uid : $scope.currentUser.uid,
           text: $scope.text
     });
+    
+    var posterUnread = $scope.chat.posterUnread;
+    var seekerUnread = $scope.chat.seekerUnread;
+    if($scope.currentUser.uid === $scope.chat.userUid) {
+        posterUnread = posterUnread + 1;
+    } else if($scope.currentUser.uid === $scope.chat.jobUid) {
+        seekerUnread = seekerUnread + 1;
+    }
+
+    //updating chat details
+    Chats.updateChat($scope.chat.$id, currTimeStamp, $scope.text, posterUnread, seekerUnread);
+    /*$scope.chat.datetime = currTimeStamp;
+    $scope.chat.lastText = $scope.text;
+    $scope.chat.$save();*/
 
     $scope.text = '';
-    viewScroll.scrollBottom(true);
+    $timeout(function() {
+        $ionicScrollDelegate.scrollBottom();
+    }, 300);
   }
 
   $scope.inputUp = function() {
     if (isIOS) $scope.data.keyboardHeight = 216;
     $timeout(function() {
-      viewScroll.scrollBottom(true);
+      $ionicScrollDelegate.scrollBottom(true);
     }, 300);
 
   };
 
   $scope.inputDown = function() {
     if (isIOS) $scope.data.keyboardHeight = 0;
-    viewScroll.resize();
+    $ionicScrollDelegate.resize();
   };
 
   $scope.closeKeyboard = function() {
     // cordova.plugins.Keyboard.close();
   };
+
+
+
+  // override soft back
+  // framework calls $rootScope.$ionicGoBack when soft back button is pressed
+  var oldSoftBack = $rootScope.$ionicGoBack;
+
+  var clearCounts  = function() {
+    if($scope.currentUser.uid === $scope.chat.userUid) {
+      if($scope.chat.seekerUnread !== 0) {
+        Chats.resetSeekerUnreadCount($scope.chat.$id);
+      }
+    } else if($scope.currentUser.uid === $scope.chat.jobUid) {
+      if($scope.chat.posterUnread !== 0) {
+        Chats.resetPosterUnreadCount($scope.chat.$id);
+      }
+    }
+    oldSoftBack();
+  };
+
+  $rootScope.$ionicGoBack = function() {
+      clearCounts();
+
+  };
+  var deregisterSoftBack = function() {
+      $rootScope.$ionicGoBack = oldSoftBack;
+  };
+
+  // override hard back
+  // registerBackButtonAction() returns a function which can be used to deregister it
+  var deregisterHardBack = $ionicPlatform.registerBackButtonAction(
+      clearCounts, 101
+  );
+
+  // cancel custom back behaviour
+  $scope.$on('$destroy', function() {
+      deregisterHardBack();
+      deregisterSoftBack();
+  });
+
+  
 
 })
--- www/js/services/chats.js
+++ www/js/services/chats.js
@@ -3,38 +3,56 @@ app.factory('Chats', function(FURL, $firebaseArray, $firebaseObject, Auth) {
   var currentUser = Auth.user;
   //var ref = new Firebase(FURL);
   var ref = firebaseInst;
-  var chatsRef = $firebaseArray(ref.child('chats'));
+  var chatsRef = ref.child('chats');
+  var chats = $firebaseArray(chatsRef);
+  var chatMessagesRef = ref.child('chat_messages');
 
   var Chats = {
 
-    all: chatsRef,
+    all: chats,
 
     createChat: function(job) {
 
-      var initialText = 'Hi i; am interested. If you like my profile please share the detals';
+      var initialText = 'Hi i am interested. If you like my profile please send me your mail id to share my resume';
 
       var chat = {
         datetime : Firebase.ServerValue.TIMESTAMP,
-        jobGravatar : job.gravatar,
-        jobPoster : job.poster,
+        //jobGravatar : job.gravatar,
+        //jobPoster : job.poster,
         jobUid : job.uid,
-        jobSkills : job.skills,
-        jobPostedOn : job.datetime,
+        //jobSkills : job.skills,
+        //jobPostedOn : job.datetime,
         gravatar : currentUser.facebook.profileImageURL,
         name : currentUser.facebook.displayName,
-        initialText : initialText,
         lastText : initialText,
-        userUid : currentUser.uid
+        userUid : currentUser.uid,
+        posterUnread : 1,
+        seekerUnread : 0
       };
-      return chatsRef.$add(chat);
+      return chats.$add(chat);
     },
 
     get: function(chatId) {
-      return $firebaseObject(ref.child('chats').child(chatId));
+      return $firebaseObject(chatsRef.child(chatId));
+    },
+
+    updateChat: function(chatId, datetime, lastText, posterUnread, seekerUnread) {
+      var chatRef = chatsRef.child(chatId);
+      chatRef.update({ 'datetime': datetime, 'lastText': lastText, 'posterUnread' : posterUnread, 'seekerUnread' :  seekerUnread });
+    },
+
+    resetPosterUnreadCount: function(chatId) {
+      var chatRef = chatsRef.child(chatId);
+      chatRef.update({ 'posterUnread' : 0 });
+    },
+
+    resetSeekerUnreadCount: function(chatId) {
+      var chatRef = chatsRef.child(chatId);
+      chatRef.update({ 'seekerUnread' : 0 });
     },
 
     getMessages: function(chatId) {
-      return $firebaseArray(ref.child('chats').child(chatId)); 
+      return $firebaseArray(chatMessagesRef.child(chatId)); 
     }
 
   };
